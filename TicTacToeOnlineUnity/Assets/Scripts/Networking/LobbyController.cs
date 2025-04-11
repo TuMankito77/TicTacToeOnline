34d33bbebebab2d25f39a18fca27a5c164a7e7e6
ivano <bolivaritoviva@gmail.com>
2025-04-11 07:07:11 -0400

    Joining and filtering lobbies test
    - Added test functions to filter lobbies and join them.

--- TicTacToeOnlineUnity/Assets/Scripts/Networking/LobbyController.cs
+++ TicTacToeOnlineUnity/Assets/Scripts/Networking/LobbyController.cs
@@ -1,16 +1,21 @@
 namespace TicTacToeOnline.Networking
 {
+    using System.Collections.Generic;
+
     using UnityEngine;
     
     using Unity.Services.Core;
     using Unity.Services.Authentication;
     using Unity.Services.Lobbies;
     using Unity.Services.Lobbies.Models;
+    using System.Collections;
 
     public class LobbyController : MonoBehaviour
     {
         private bool isPlayerSignedIn = false;
-        private bool wasLobbyCreated = false;
+        private Lobby lobbyCreated = null;
+        private Lobby lobbyJoined = null;
+        private Coroutine keepLobbyAliveCoroutine;
 
         #region Unity Methods
 
@@ -26,15 +31,24 @@ namespace TicTacToeOnline.Networking
         {
             if(isPlayerSignedIn)
             {
-                if(Input.GetKeyDown(KeyCode.Space) && !wasLobbyCreated)
+                if(Input.GetKeyDown(KeyCode.Space) && lobbyCreated == null)
                 {
                     CreateLobby();
-                    wasLobbyCreated = true;
                 }
 
-                if(Input.GetKeyDown(KeyCode.L))
+                if(Input.GetKeyDown(KeyCode.L) && lobbyJoined == null)
                 {
                     ListLobbies();
+                }
+
+                if(Input.GetKeyDown(KeyCode.J) && lobbyJoined == null)
+                {
+                    ConnectToFirstLobbyFound();
+                }
+
+                if(Input.GetKeyDown(KeyCode.Q) && lobbyJoined == null)
+                {
+                    QuickJoinLobby();
                 }
             }
         }
@@ -52,13 +66,14 @@ namespace TicTacToeOnline.Networking
             try
             {
                 string lobbyName = "My lobby";
-                int maxPlayers = 4;
-                Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
-                Debug.Log($"Lobby created successfully! Lobby name: {lobby.Name} - Max number of players: {lobby.MaxPlayers}");
+                int maxPlayers = 2;
+                lobbyCreated = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+                keepLobbyAliveCoroutine = StartCoroutine(KeepLobbyAlive());
+                Debug.Log($"Lobby created successfully! Lobby name: {lobbyCreated.Name} - Max number of players: {lobbyCreated.MaxPlayers}");
             }
             catch(LobbyServiceException e)
             {
-                Debug.Log(e);
+                Debug.LogError(e);
             }
         }
 
@@ -66,7 +81,20 @@ namespace TicTacToeOnline.Networking
         {
             try
             {
-                QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+                QueryLobbiesOptions queryLobbyOptions = new QueryLobbiesOptions
+                {
+                    Count = 25,
+                    Filters = new List<QueryFilter>
+                    {
+                        new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "1", QueryFilter.OpOptions.GE)
+                    },
+                    Order = new List<QueryOrder>
+                    {
+                        new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                    }
+                };
+
+                QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(queryLobbyOptions);
                 Debug.Log($"Lobbies found: {queryResponse.Results.Count}");
 
                 foreach(Lobby lobby in queryResponse.Results)
@@ -76,7 +104,66 @@ namespace TicTacToeOnline.Networking
             }
             catch(LobbyServiceException e)
             {
-                Debug.Log(e);
+                Debug.LogError(e);
+            }
+        }
+
+        private async void ConnectToFirstLobbyFound()
+        {
+            try
+            {
+                QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+                Debug.Log($"Lobbies found: {queryResponse.Results.Count}");
+
+                if(queryResponse.Results.Count <= 0)
+                {
+                    Debug.Log($"No lobbies were found :(");
+                    return;
+                }
+
+                lobbyJoined = await LobbyService.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+
+                Debug.Log($"Joined to lobby successfully! Lobby name: {lobbyJoined.Name} - Max number of players: {lobbyJoined.MaxPlayers}");
+            }
+            catch(LobbyServiceException e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+
+        private IEnumerator KeepLobbyAlive()
+        {
+            yield return new WaitForSeconds(15);
+
+            if(lobbyCreated != null)
+            {
+                SendLobbyHeartbeat();
+            }
+        }
+
+        private async void SendLobbyHeartbeat()
+        {
+            try
+            {
+                await LobbyService.Instance.SendHeartbeatPingAsync(lobbyCreated.Id);
+                keepLobbyAliveCoroutine = StartCoroutine(KeepLobbyAlive());
+                Debug.Log("Heartbeat sent");
+            }
+            catch(LobbyServiceException e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+
+        private async void QuickJoinLobby()
+        {
+            try
+            {
+                lobbyJoined = await LobbyService.Instance.QuickJoinLobbyAsync();
+            }
+            catch(LobbyServiceException e)
+            {
+                Debug.LogError(e.Message);
             }
         }
     }
